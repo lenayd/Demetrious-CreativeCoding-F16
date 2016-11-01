614d8feac5b6d061ece7ddb6bbd261b4c0227ebb
Lenay Demetrious <ldemetrious@eg.poly.edu>
2016-10-31 20:06:56 -0400

    Add files via upload

--- Grid Patterns.js
+++ Grid Patterns.js
@@ -13,9 +13,13 @@ function setup() {
 }
 
 function draw() {
+  background(255);
   myGrid.animate(); 
+  myGrid.show(); 
   myGrid2.animate(); 
+  myGrid2.show(); 
   myGrid3.animate(); 
+  myGrid3.show(); 
 }
 
 function Grid(scale, speed, red, green, blue, xtranslation, ytranslation, rotation){
@@ -30,17 +34,21 @@ function Grid(scale, speed, red, green, blue, xtranslation, ytranslation, rotati
 	this.xtran = xtranslation; 
 	this.ytran = ytranslation; 
 	this.animate = function(){
-	push(); 
-	translate(this.xtran, this.ytran); 
-	rotate(this.rot); 
+		this.x += this.vel; 
+		this.y += this.vel; 
+		if(this.x >= width || this.x <= 0 || this.y >= height || this.y <= 0){
+			this.vel = -this.vel; 
+		}
+	}
+	this.show = function(){
 		for(var i = 0; i < 50; i += 1){
 			for(var j = 0; j < 50; j += 1){
 				fill(this.r, this.g, this.b); 
 				ellipse(this.x+(i*this.vel), this.y+(j*this.vel), this.size, this.size); 
 			}	
 		}
-	pop(); 
 	}
-}
+
+}; 
 
 
